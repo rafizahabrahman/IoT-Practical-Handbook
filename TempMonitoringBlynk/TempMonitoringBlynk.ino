1ebe0dc823d1ec6e06618cc669128a75ba0d2e7c
rafizahabrahman <fizdanae2012@gmail.com>
2024-04-18 09:29:14 +0800

    Update TempMonitoringBlynk.ino

--- TempMonitoringBlynk/TempMonitoringBlynk.ino
+++ TempMonitoringBlynk/TempMonitoringBlynk.ino
@@ -1,3 +1,8 @@
+/* Fill in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID "TMPL6b-30xqrI"
+#define BLYNK_TEMPLATE_NAME "Temperature Monitoring"
+#define BLYNK_AUTH_TOKEN "0gVYxFAcv-074X82yK9c8x8ZE8gkn_64"
+
 //DHT11 and NodeMCU with Blynk
 //----------------------------
 //Libraries used
@@ -12,10 +17,6 @@
 //Blynk Setup
 //------------
 #define BLYNK_PRINT Serial
-/* Fill in information from Blynk Device Info here */
-#define BLYNK_TEMPLATE_ID "TMPL6b-30xqrI"
-#define BLYNK_TEMPLATE_NAME "Temperature Monitoring"
-#define BLYNK_AUTH_TOKEN "0gVYxFAcv-074X82yK9c8x8ZE8gkn_64"
 BlynkTimer timer;
 
 //ThingSpeak Setup
